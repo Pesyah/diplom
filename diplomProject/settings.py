44be6ef0314530982cc6b613d950a5696baee67f
Artur <61554281+Pesyah@users.noreply.github.com>
2024-02-27 17:42:51 +0300

    fix

--- diplomProject/settings.py
+++ diplomProject/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'iriy.online@gmail.com'
-EMAIL_HOST_PASSWORD = 'dpiwmipxazwqzanh'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587       
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -97,8 +97,8 @@ DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'localhost',
-        'NAME': 'diplomdb',
-        'PORT': 2023,
+        'NAME': 'postgres',
+        'PORT': 5433,
         'USER': 'postgres',
         'PASSWORD': 'postgres'
     }
